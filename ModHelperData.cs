7764fb98a4aa9ca98f41c027048c51ef09ee0c16
TheStraying11 <jtrycroft@gmail.com>
2023-08-06 02:53:46 +0100

    update ModHelperData.cs and README.md

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "RBETracker";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Tracks your current pops against the total RBE of the mode";
 
-    public const string RepoOwner = "TheStraying11"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "RBETracker"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "TheStraying11";
+    public const string RepoName = "RBETracker";
 }
